42a85d387e3b4e10e657af28849d038809c8bb5a
Erlwein, Björn <bjoern.erlwein@huk-coburg.de>
2023-03-29 16:21:08 +0200

    Initial working draft

---
+++ src/plugins/validate-spectral/index.js
@@ -0,0 +1,50 @@
+// Base validate plugin that provides a placeholder `validateSpec` that fires
+// after `updateJsonSpec` is dispatched.
+
+import { useSelector } from "react-redux";
+
+export const updateJsonSpec = (ori, { specActions }) => (...args) => {
+    ori(...args)
+
+    const [spec] = args
+    specActions.validateSpec(spec)
+}
+
+//eslint-disable-next-line no-unused-vars
+export const validateSpec = (jsSpec) => (arg) => {
+    fetch("http://localhost:8080/oas-validation/api/validate?ruleset=v5", {
+        method: "POST",
+        headers: {
+            "Accept": "application/json"
+        },
+        body: arg.specSelectors.specStr(),
+    }).then(response => response.json()).then(response => {
+        const errors = response.map((entry) => {
+            return {
+                level: "error",
+                message: entry.message,
+                path: entry.path,
+                line: entry.range.start.line
+            }
+        })
+        arg.errActions.newSpecErrBatch(errors);
+    }
+    ).catch((error) => {
+        console.error("Error:", error);
+    });
+}
+
+export default function() {
+    return {
+        statePlugins: {
+            spec: {
+                actions: {
+                    validateSpec,
+                },
+                wrapActions: {
+                    updateJsonSpec
+                }
+            }
+        }
+    }
+}
